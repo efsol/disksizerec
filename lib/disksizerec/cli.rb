3c14b1cc2bdd6fdc04108b99bfb3772fe3190395
Eiichi Shimotori <eiichi.shimotori@gmail.com>
2015-02-20 22:31:38 +0900

    Implemented store_sizes method

--- lib/disksizerec/cli.rb
+++ lib/disksizerec/cli.rb
@@ -3,6 +3,7 @@
 require 'thor'
 require 'ohai'
 require 'mongo'
+include Mongo
 
 
 module Disksizerec
@@ -12,19 +13,21 @@ module Disksizerec
 
 
     desc "exec", "Check the sizes of mounted disks and store them to a host or just display them"
-    option :store, type: :string, desc: 'MongoDB server info to store data (host[:port])', default: ''
+    option :store, type: :string, desc: 'MongoDB host name', default: ''
+    option :port, type: :numeric, desc: 'MongoDB port', default: 27017
     option :db, type: :string, desc: 'DB name to store data', default: ''
     def exec
       store = options[:store]
+      port = options[:port]
       db = options[:db]
-      puts "exec with #{store}"
+      # puts "exec with #{store}"
       sizes = get_disk_sizes
       puts sizes.inspect
       
       return  if store == ''
       abort 'Error: DB name should be specified with --db option'  if db == ''
       
-      store_sizes sizes, store, db
+      store_sizes store, port, db, sizes
     end
     
     
@@ -35,28 +38,30 @@ module Disksizerec
     def get_disk_sizes
       oh = Ohai::System.new
       oh.all_plugins
-      sizes = {}
+
+      sizes = []
       oh[:filesystem].each do |key, value|
-        #puts "#{value[:mount]}, #{value[:kb_used]}, #{value[:kb_available]}"  if value.key?(:kb_used)
         next  unless value.key?(:kb_used)
 
-        mount = value[:mount]
-        size = { 
+        size = {
+            mount: value[:mount],
             mb_used: value[:kb_used].to_i / 1024, 
-            mb_available: value[:kb_available].to_i / 1024
+            mb_available: value[:kb_available].to_i / 1024,
+            created_at: Time.now
         }
-        
-        sizes[mount] = size
+        sizes << size
       end
-
       sizes
     end
     
     
     # Store size data
-    def store_sizes(sizes, store, db)
-      puts "store_sizes to #{store}, #{db}"
-      
+    def store_sizes(store, port, db, sizes)
+      # puts "store_sizes to #{store}, #{db}"
+      client = MongoClient.new store, port
+      db = client.db db
+      coll = db.collection 'disk_sizes'
+      coll.insert sizes
     end
 
   end
